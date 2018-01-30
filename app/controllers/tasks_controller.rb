e1c232ccc32a5e285f2a0461690a0cf3fbc8d416
asmentko <asmentko@gmail.com>
2018-01-30 02:07:17 +0000

    Projects and tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,17 @@ class TasksController < ApplicationController
   before_action :set_project
 
   def create
-    @task = @project.task.create(task_params)
+    @task = @project.tasks.create(task_params)
+    redirect_to @project
+  end
 
+  def destroy
+    @task = @project.tasks.find(params[:id])
+    if @task.destroy
+      flash[:success] = "Task was deleted"
+    else
+      flash[:error] = "Task was not deleted"
+    end
     redirect_to @project
   end
 
@@ -14,7 +23,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params[:task].permit(:content))
+    params[:task].permit(:content)
   end
 
 end
